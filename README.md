21e4f27b55b894dc7dccb0b13ca81952eef04f6f
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-02-18 17:09:25 -0300

    variables de entorno desde el servidor y un botón de compra en la página de libro, junto con la integración de un logo de Amazon

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Tutorial de Astro 5 con Server Islands impartido por MiduDev
 
 [Siguiendo este tutorial de MiduDev](https://youtu.be/WHqZAXHZN_w?si=xfsdsBFB24irJrOp)
+
+## npx astro sync
+para sincronizar las colecciones
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,5 +7,17 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public' }),
+      SCORE_API_ENDPOINT: envField.string({ default: '3', context: 'client', access: 'public' }),
+    }
   }
 });
+// el env: lee las variables de entorno y las pone en el objeto global .env
+// el schema: define las variables de entorno que se pueden usar en el proyecto
+// el envField: define el tipo de variable de entorno que se va a usar
+// default: valor por defecto
+// context: donde se va a usar la variable de entorno
+// access: si es publica o privada
